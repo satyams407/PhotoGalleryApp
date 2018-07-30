6941f7ee817a2007784bdc2078ffa95db81d1d7c
Satyam Sehgal <satyams407@gmail.com>
2018-07-30 11:53:12 +0530

    Added table

---
+++ PhotoGalleryApp/Presentation Layer/Cells/PhotoGalleryCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoGalleryCell.swift
+//  PhotoGalleryApp
+//
+//  Created by Satyam Sehgal on 29/07/18.
+//  Copyright © 2018 Satyam Sehgal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class PhotoGalleryCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var imageview: UIImageView!
+
+    
+}
--- PhotoGalleryApp/Presentation Layer/ViewControllers/LandingViewController.swift
+++ PhotoGalleryApp/Presentation Layer/ViewControllers/LandingViewController.swift
@@ -7,16 +7,29 @@ import UIKit
 
 class LandingViewController: UIViewController {
 
+    var dataSource: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createDataSource()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createDataSource() {
+       self.dataSource = ["first","second"]
     }
+}
 
+//MARK: TableView datasource and delegate methods
+extension LandingViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "PhotoGalleryCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier)
 
+        cell?.imageView?.image = #imageLiteral(resourceName: "circle")
+        return cell!
+    }
 }
 
