3bf766d331402e1ca3f378251e189fb4ad761e48
Satyam Sehgal <satyam.sehgal@nagarro.com>
2019-01-29 10:51:09 +0530

    Intermediate changes of login functionality

---
+++ PhotoGalleryApp/Presentation Layer/ViewControllers/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  PhotoGalleryApp
+//
+//  Created by Satyam Sehgal on 28/01/19.
+//  Copyright © 2019 Satyam Sehgal. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var userName: UIView!
+    @IBOutlet weak var password: UITextField!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+    }
+    
+}
---
+++ PhotoGalleryApp/Presentation Layer/ViewModel/SearchViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewModel.swift
+//  PhotoGalleryApp
+//
+//  Created by Satyam Sehgal on 20/01/19.
+//  Copyright © 2019 Satyam Sehgal. All rights reserved.
+//
+
+import Foundation
